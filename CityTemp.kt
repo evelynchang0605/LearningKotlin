f03e8e5ed40bf50f6e988d288dbac6e790d4f1ed
Evelyn <137132532+evelynchang0605@users.noreply.github.com>
2023-06-29 09:44:23 +0800

    Update CityTemp.kt

--- CityTemp.kt
+++ CityTemp.kt
@@ -10,3 +10,21 @@ fun main() {
 fun cityTemp(City: String, l_temp: Int, h_temp: Int, rain: Int){
     println("City: $City \nLow temperature: $l_temp, High temperature: $h_temp \nChance of rain: $rain% \n")
 }
+
+/* Print result
+City: Ankara 
+Low temperature: 27, High temperature: 31 
+Chance of rain: 82% 
+
+City: Tokyo 
+Low temperature: 32, High temperature: 36 
+Chance of rain: 10% 
+
+City: Cape Town 
+Low temperature: 59, High temperature: 64 
+Chance of rain: 2% 
+
+City: Guatemala City 
+Low temperature: 50, High temperature: 55 
+Chance of rain: 7% 
+*/
